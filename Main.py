f50a2ed02d6a21e7c8628ff7f3eaf43ec9b97e15
ainaajidian <ainaajidian@gmail.com>
2025-05-20 11:00:41 +0700

    Normalize Data

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 from LoadData import load_data
 from CleanData import clean_data
+from NormalizeData import normalize_data
 
 def main():
     customer, sales, product = load_data()
     customer, sales, product = clean_data(customer, sales, product)
+    customer, sales, product = normalize_data(customer, sales, product)
 
 if __name__ == "__main__":
     main()
---
+++ NormalizeData.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def normalize_data(customer, sales, product):
+    sales = sales.rename(columns={
+        'user id': 'Customer ID',
+        'product id': 'Product ID',
+        'Time stamp': 'Order Date'
+    })
+
+    sales.dropna(subset=['Customer ID', 'Product ID'], inplace=True)
+    sales['Order Date'] = pd.to_datetime(sales['Order Date'], errors='coerce')
+    sales['Customer ID'] = pd.to_numeric(sales['Customer ID'], errors='coerce')
+    sales['Product ID'] = sales['Product ID'].astype(str)
+    customer['Age'] = pd.to_numeric(customer['Age'], errors='coerce')
+    customer['Purchase Amount (USD)'] = pd.to_numeric(customer['Purchase Amount (USD)'], errors='coerce')
+    customer['Previous Purchases'] = pd.to_numeric(customer['Previous Purchases'], errors='coerce')
+
+    print("\nData normalized.")
+    return customer, sales, product
